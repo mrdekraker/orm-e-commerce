08163b24e51015f9c35bab52d3ca3f933d26cae9
mrdekraker <30578794+mrdekraker@users.noreply.github.com>
2022-11-10 17:45:59 -0600

    product-routes edited

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -80,11 +80,19 @@ router.post('/', (req, res) => {
 // update product
 router.put('/:id', (req, res) => {
   // update product data
-  Product.update(req.body, {
-    where: {
-      id: req.params.id,
+  Product.update(
+    {
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
     },
-  })
+    {
+      where: {
+        id: req.params.id,
+      },
+    }
+  )
     .then((product) =>
       // find all associated tags from ProductTag
       ProductTag.findAll({ where: { product_id: req.params.id } })
